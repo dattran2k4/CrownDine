fb61b3bb459be41e81b0da62ad69bf76590b906f
dattran2k4 <tranquocthanhdat@gmail.com>
2026-01-26 22:36:46 +0700

    Setup base config cloudinary

--- backend/src/main/java/com/crowndine/controller/ApiUserController.java
+++ backend/src/main/java/com/crowndine/controller/ApiUserController.java
@@ -1,5 +1,7 @@
 package com.crowndine.controller;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.crowndine.dto.request.ChangePasswordRequest;
 import com.crowndine.dto.response.ApiResponse;
 import com.crowndine.service.user.UserService;
@@ -8,6 +10,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Map;
 
 @RestController
 @Validated
@@ -17,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 public class ApiUserController {
 
     private final UserService userService;
+    private final Cloudinary cloudinary;
 
     @GetMapping
     public ApiResponse getListUsers() {
@@ -30,5 +38,19 @@ public class ApiUserController {
     @PostMapping("change-password")
     public ApiResponse changePassword(@Valid @RequestBody ChangePasswordRequest changePasswordRequest) {
         return ApiResponse.builder().build();
+    }
+
+    @PutMapping("upload-avatar")
+    public ApiResponse uploadAvatar(@RequestParam("image") MultipartFile file, Principal principal) throws IOException {
+        Map params = ObjectUtils.asMap(
+                "folder", "crown_dine_avatars",
+                "resource_type", "image"
+        );
+        Map result = cloudinary.uploader().upload(file.getBytes(), params);
+        return ApiResponse.builder()
+                .status(200)
+                .message("Update avatar successfully")
+                .data(String.valueOf(result.get("secure_url")))
+                .build();
     }
 }

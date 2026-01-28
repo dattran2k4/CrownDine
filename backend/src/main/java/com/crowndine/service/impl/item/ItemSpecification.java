28cb320301498c48ef3fff8b5e187812f268831a
dattran2k4 <tranquocthanhdat@gmail.com>
2026-01-28 15:58:27 +0700

    Add search by combo name, status ACTIVE

--- backend/src/main/java/com/crowndine/service/impl/item/ItemSpecification.java
+++ backend/src/main/java/com/crowndine/service/impl/item/ItemSpecification.java
@@ -1,5 +1,8 @@
 package com.crowndine.service.impl.item;
 
+import com.crowndine.common.enums.EItemStatus;
+import com.crowndine.model.Combo;
+import com.crowndine.model.ComboItem;
 import com.crowndine.model.Item;
 import jakarta.persistence.criteria.*;
 import org.springframework.data.jpa.domain.Specification;
@@ -20,10 +23,17 @@ public class ItemSpecification {
                 predicate = cb.and(cb.equal(root.get("category").get("id"), categoryId));
             }
 
+            predicate = cb.and(cb.equal(root.get("status"), EItemStatus.AVAILABLE));
+
             if (StringUtils.hasLength(search)) {
                 String pattern = String.format("%%%s%%", search.trim().toLowerCase());
+
+                Join<Item, ComboItem> comboItemJoin = root.join("comboItems", JoinType.LEFT);
+
+                Join<ComboItem, Combo> comboJoin = comboItemJoin.join("combo", JoinType.LEFT);
                 predicate = cb.and(predicate, cb.or(
-                        cb.like(cb.lower(root.get("name")), pattern)
+                        cb.like(cb.lower(root.get("name")), pattern),
+                        cb.like(cb.lower(comboJoin.get("name")), pattern)
                 ));
             }
             return predicate;

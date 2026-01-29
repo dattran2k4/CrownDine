a78021758d98d0a2662430ca5505151cddb34e1a
dattran2k4 <tranquocthanhdat@gmail.com>
2026-01-29 15:28:20 +0700

    prettier for front end

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,9 +1,9 @@
-import { LoginForm } from "@/components/form/login-form"
+import { LoginForm } from '@/components/form/login-form'
 
 const Login = () => {
   return (
-    <section className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10 bg-gradient-orange">
-      <div className="w-full max-w-md">
+    <section className='bg-muted bg-gradient-orange flex min-h-svh flex-col items-center justify-center p-6 md:p-10'>
+      <div className='w-full max-w-md'>
         <LoginForm />
       </div>
     </section>
--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -1,9 +1,9 @@
-import { SignupForm } from "@/components/form/signup-form"
+import { SignupForm } from '@/components/form/signup-form'
 
 const Register = () => {
   return (
-    <section className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10 bg-gradient-orange">
-      <div className="w-full max-w-md">
+    <section className='bg-muted bg-gradient-orange flex min-h-svh flex-col items-center justify-center p-6 md:p-10'>
+      <div className='w-full max-w-md'>
         <SignupForm />
       </div>
     </section>

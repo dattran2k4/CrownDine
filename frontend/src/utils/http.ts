a08eb5b3291c31e693f560c556638448ac3bd471
Tran Duc <duct23333@gmail.com>
2026-02-26 00:53:55 +0700

    fix

--- frontend/src/utils/http.ts
+++ frontend/src/utils/http.ts
@@ -15,10 +15,11 @@ class Http {
 
     this.instance.interceptors.request.use(
       (config) => {
-        if (this.accessToken && config.headers) {
-          config.headers.Authorization = this.accessToken.startsWith('Bearer ') 
-            ? this.accessToken 
-            : `Bearer ${this.accessToken}`
+        const { accessToken } = useAuthStore.getState()
+        if (accessToken && config.headers) {
+          config.headers.Authorization = accessToken.startsWith('Bearer ') 
+            ? accessToken 
+            : `Bearer ${accessToken}`
         }
         return config
       },

fad933e8602721b9fffcb2b8cb2d5d569d6f8656
Tran Duc <duct23333@gmail.com>
2026-03-16 22:01:54 +0700

    fix_conflict

--- frontend/src/apis/voucher.api.ts
+++ frontend/src/apis/voucher.api.ts
@@ -6,8 +6,6 @@ import type {
   VoucherValidateRequest,
   VoucherValidateResponse
 } from '@/types/voucher.type'
-import type { VoucherValidateRequest, VoucherValidateResponse } from '@/types/voucher.type'
-import type { ApiResponse } from '@/types/utils.type'
 import http from '@/utils/http'
 
 const VOUCHER_URL = '/vouchers'
@@ -61,7 +59,6 @@ const voucherApi = {
     return http.get<ApiResponse<VoucherAssignmentResponse[]>>(`${VOUCHER_URL}/${voucherId}/assignments`)
   },
 
-const voucherApi = {
   validateVoucher(data: VoucherValidateRequest) {
     return http.post<ApiResponse<VoucherValidateResponse>>(`${VOUCHER_URL}/validate`, data)
   }

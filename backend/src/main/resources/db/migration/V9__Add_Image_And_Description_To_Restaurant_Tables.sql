12c43a3757bb6d824f083d4de201573e133acd8f
dattran2k4 <tranquocthanhdat@gmail.com>
2026-04-06 14:19:27 +0700

    update db/migartion

--- backend/src/main/resources/db/migration/V9__Add_Image_And_Description_To_Restaurant_Tables.sql
+++ backend/src/main/resources/db/migration/V9__Add_Image_And_Description_To_Restaurant_Tables.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `restaurant_tables`
-    ADD COLUMN IF NOT EXISTS `image_url` VARCHAR(255) NULL,
-    ADD COLUMN IF NOT EXISTS `description` TEXT NULL;
+    ADD COLUMN `image_url` VARCHAR(255) NULL,
+    ADD COLUMN `description` TEXT NULL;
